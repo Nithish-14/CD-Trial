4c24139d840982e616f3aeb36341be8eb279d1c1
Nithish <nithishm612345@gmail.com>
2024-05-29 23:09:37 +0530

    update1

--- part-2-scripts/script-local.sh
+++ part-2-scripts/script-local.sh
@@ -1,1 +1,1 @@
-ssh -t -i "cert-file.cer" ubuntu@ec2-url  "sudo bash ~/deploy.sh"
+ssh -t -i "todo-app-kp.pem" ubuntu@http://ec2-13-126-59-165.ap-south-1.compute.amazonaws.com  "sudo bash ~/deploy.sh"
