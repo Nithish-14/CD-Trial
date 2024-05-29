ee8f3b97318e18d47263a0fd003b440792cb11fc
Nithish <nithishm612345@gmail.com>
2024-05-29 23:47:39 +0530

    update i

--- part-2-scripts/script-local.sh
+++ part-2-scripts/script-local.sh
@@ -1,1 +1,1 @@
-ssh -t -i "todo-app-kp.pem" ubuntu@ec2-13-126-59-165.ap-south-1.compute.amazonaws.com  "source ./deploy.sh"
+ssh -t -i "todo-app-kp.pem" ubuntu@ec2-13-126-59-165.ap-south-1.compute.amazonaws.com  "sudo bash ~/deploy.sh"
